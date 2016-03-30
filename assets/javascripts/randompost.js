5b3fa1f5b503715d6c86739449dc337f471cbb81
Michael Boumann <mboumann@outlook.com>
2016-03-30 23:26:13 +0200

    random post limit back to 2

--- assets/javascripts/randompost.js
+++ assets/javascripts/randompost.js
@@ -8,7 +8,7 @@
     function onSuccess(data) {
         var $result = $('#random-posts-list');
         var sortedPosts = shuffleArray(data.posts);
-        var displayPosts = sortedPosts.slice(0,3);
+        var displayPosts = sortedPosts.slice(0,2);
         $.each(displayPosts, function (i, post) {
             $result.append(
                 '<a href="https://www.miboutech.com' + post.url + '"><img class="article-footer-img" src="' + post.image + '" title="' + post.title + '"/></a>'
