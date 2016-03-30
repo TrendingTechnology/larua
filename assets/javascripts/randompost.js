6c58d662f20b9a27686ae7f3ade7bffc7604315e
Michael Boumann <mboumann@outlook.com>
2016-03-30 23:23:32 +0200

    change to article-footer-img

--- assets/javascripts/randompost.js
+++ assets/javascripts/randompost.js
@@ -11,7 +11,7 @@
         var displayPosts = sortedPosts.slice(0,3);
         $.each(displayPosts, function (i, post) {
             $result.append(
-                '<a href="https://www.miboutech.com' + post.url + '"><img class="sidebar-img" src="' + post.image + '" title="' + post.title + '"/></a>'
+                '<a href="https://www.miboutech.com' + post.url + '"><img class="article-footer-img" src="' + post.image + '" title="' + post.title + '"/></a>'
             );
         });
     }
