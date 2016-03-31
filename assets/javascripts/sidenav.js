8ba8ae46846e00f7457e008128c377338046fb87
Michael Boumann <mboumann@outlook.com>
2016-03-31 22:46:25 +0200

    keep default width for side menu

--- assets/javascripts/sidenav.js
+++ assets/javascripts/sidenav.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   // Activate the side menu
   // Initialize collapse button
   $('.button-collapse').sideNav({
-        menuWidth: 300, // Default is 240
         edge: 'right', // Choose the horizontal origin
         closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
       }
