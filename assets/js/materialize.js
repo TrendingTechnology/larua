a3c04d8836eef9c323ed77b12f0a9548ade5402c
Michael Boumann <mboumann@outlook.com>
2016-04-22 17:36:55 +0200

    combine sidenav and slider js

---
+++ assets/js/materialize.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  // Activate the side menu
+  // Initialize collapse button
+  $('.button-collapse').sideNav({
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      }
+    );
+
+    $('.slider').slider({indicators: false});
+ });
