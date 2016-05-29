2c71cf6d4acae7d5568056c2c2fcafe9a808bd2f
Michael Boumann <mboumann@outlook.com>
2016-05-29 16:10:47 +0200

    add li class

--- assets/js/randompost.js
+++ assets/js/randompost.js
@@ -11,7 +11,7 @@
         var displayPosts = sortedPosts.slice(0,2);
         $.each(displayPosts, function (i, post) {
             $result.append(
-                '<a href="' + post.url + '"><img class="article-footer-img" src="' + post.image + '" title="' + post.title + '"/></a>'
+                '<li class="collection-item"><a href="' + post.url + '">' + post.title + '</a></li>';
             );
         });
     }
