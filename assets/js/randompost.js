839acd1d77d67dbac438773263858f5603f5a639
Michael Boumann <mboumann@outlook.com>
2016-05-29 16:23:06 +0200

    bump number of posts up with 1

--- assets/js/randompost.js
+++ assets/js/randompost.js
@@ -8,7 +8,7 @@
     function onSuccess(data) {
         var $result = $('#random-posts-list');
         var sortedPosts = shuffleArray(data.posts);
-        var displayPosts = sortedPosts.slice(0,2);
+        var displayPosts = sortedPosts.slice(0,3);
         $.each(displayPosts, function (i, post) {
             $result.append(
                 '<a href="' + post.url + '"><img class="article-footer-img" src="' + post.image + '" title="' + post.title + '"/></a>'
