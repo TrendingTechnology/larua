724160b7a1a0a7b805e42d14c7eb0289a64651cd
Michael Boumann <mboumann@outlook.com>
2016-05-29 23:03:32 +0200

    remove comma

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   // Initialize collapse button
   $('.button-collapse').sideNav({
         edge: 'right', // Choose the horizontal origin
-        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
       }
     );
 
